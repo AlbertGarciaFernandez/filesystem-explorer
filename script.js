e6d48034b4be1eeb025f7e98045d7d44021655d7
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-12-21 13:04:10 +0000

    Subir arc hivos desde el pc dentro de una carpeta seleccionada

--- script.js
+++ script.js
@@ -6,24 +6,36 @@ function printModal(){
     emptyModal();
     switch (this.value) {
         case 'addFile':
-            $(".modal-body").append('<form method="post" action="file_operation.php" id="create_form">'+
-            '<input type="text" name="file_name">'+
+            $('#exampleModalLabel').append(this.value);
+            $(".modal-body").append('<form method="post" action="file_operation.php" enctype= "multipart/form-data">'+
+            '<label>Nombre de la carpeta</label>'+
+            '<input type="text" name="carpeta">'+
+            '<input type="file" name="archivo">'+
             '<input type="submit" value="Create File" name="create_file">'+
             '</form>');
         break;
         case 'deleteFile':
+            $('#exampleModalLabel').append(this.value);
             $(".modal-body").append('<form method="post" action="file_operation.php" id="delete_form">'+
             '<input type="text" name="file_name">'+
             '<input type="submit" value="Delete File" name="delete_file">'+
             '</form>');
         break;
         case 'editFile':
+            $('#exampleModalLabel').append(this.value);
             $('.modal-body').append('<form method="post" action="file_operation.php" id="edit_form">'+
             '<input type="text" name="file_name">'+
             '<textarea name="edit_text"></textarea>'+
             '<input type="submit" value="Edit File" name="edit_file">'+
-           '</form>')
+            '</form>')
         break;
+        case 'addDir':
+            $('#exampleModalLabel').append(this.value);
+            $(".modal-body").append('<form method="post" action="file_operation.php" id="dir_form">'+
+            '<input type="text" name="dir_name">'+
+            '<input type="submit" value="Create Dir" name="create_dir">'+
+            '</form>');
+            break;
                 default:
             break;
     }
@@ -31,5 +43,6 @@ function printModal(){
 
 
 function emptyModal(){
+    $('#exampleModalLabel').empty();
     $('.modal-body').empty();
 }
